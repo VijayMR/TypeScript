60bfb9d320e974bdb41b964ae31fb7bef5cdcb8d
Vijay MR <vijay.mr@tavant.com>
2026-07-08 12:15:16 +0530

    ConditionalStatements

---
+++ tests/ConditionalStatements.ts
@@ -0,0 +1,42 @@
+console.log("=>If conditional statment");
+let a:number=10;
+if(a>5){
+    console.log("Working as expected");
+}
+/////////////////////////////////////////////
+console.log("=>If & else conditional statment");
+let b:number=5;
+if(b>5){
+    console.log("Expected Value is 5");
+}
+else{
+    console.log("Expected Value did not appear")
+}
+//////////////////////////////////////////////
+console.log("=>If , else if , else conditional statment");
+let c:number=5;
+if(c>5){
+    console.log("Expected Value is 5");
+}
+else if(c=5){
+console.log("Exactly matched the same value 5")
+}
+else{
+    console.log("Expected Value did not appear")
+}
+//////////////////////////////////////////////
+console.log("=> Switch cases")
+let d:string="Car";
+switch(d){
+    case "bike":
+        console.log("It is bike");
+        break;
+    
+    case "Car":
+        console.log("It is Car");
+        break;
+
+    default:
+        console.log("It is Vehicle");
+}
+/////////////////////////////////////////////
