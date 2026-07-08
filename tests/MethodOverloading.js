a1e47d19ecd9d3e3f496e4aa97d58cc697f1cb1f
Vijay MR <vijay.mr@tavant.com>
2026-07-08 11:58:51 +0530

    UpdatedBranch

--- tests/MethodOverloading.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//Method overloading 
-function add(x, y) {
-    console.log(`The Value of ${x} and ${y}`);
-    return x + y;
-}
-console.log(add(1, 2));
-console.log(add("Try", "New"));
--- tests/basic.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-let a = 25;
-console.log(a);
