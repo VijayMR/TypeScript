469ea29e30344ea3c48580e55b0da17646d5524f
Vijay MR <vijay.mr@tavant.com>
2026-07-09 19:18:23 +0530

    CallBackFunction&MethodOverloading

---
+++ tests/CallBackFunction.ts
@@ -0,0 +1,26 @@
+//Call Back Function - calling one function in other function to execute later.
+
+//Example:
+
+function expected(a:number,b:number,callback:(name:string)=>void,callback1:(number:number)=>void){ 
+    
+    //callback:(name:string)=>void -->Syntax
+    //callback is not keyword it is just the variable
+
+    let count=a+b;
+    console.log("The total count is", count)
+    //other functions getting called as
+    callback("John")
+    callback1(count);
+}
+
+//call back function1
+function greet(name:string){
+    console.log(`The name is ${name}`)
+}
+//call back function2
+function greet1(number:number){
+    console.log(`The number is ${number}`)
+}
+//function invoking
+expected(5,5,greet,greet1) //---greet,greet1 is the function name
---
+++ tests/FunctionOverloading.ts
@@ -0,0 +1,61 @@
+//For function overloading 3 steps to be followed as mandatory....
+//----> Same class, same function, but different parameters (i.e can be different count of same parameter,different parameter datatype, 
+// different return types)
+
+//-->Step 1 : Write all the signature (Without the implementation)
+//-->Step 2 : Write the implementation
+//-->Step 3 : Call the implementation
+
+console.log("==>Different parameters and same return type")
+//Signature creation
+function getdata(a:number):string;
+function getdata(a:string):string;
+
+//implementation
+function getdata(para: number|string):string{
+    if(typeof para === "number"){
+        return (`It is a number : ${para}`);
+    }
+    else{
+        return (`It is a string : ${para}`)
+    }
+}
+
+//calling implementation
+console.log(getdata("Hello World"));
+console.log(getdata(100));
+
+console.log("==>same parameters but count different and same return type")
+function getvalue(a:number):number;
+function getvalue(a:number,b:number):number;
+
+function getvalue(a:number,b?:number):number{
+    if(typeof b === "undefined"){
+        return a;
+    }
+
+    return a+b;
+}
+
+console.log(getvalue(10));
+console.log(getvalue(10,20));
+
+console.log("==>Different parameters and different return type")
+//Signature creation
+function getdatascore(a:number):number;
+function getdatascore(b:string):string;
+
+//implementation
+function getdatascore(para: number|string):string|number{
+    if(typeof para === "number"){
+        return (`It is a number : ${para}`);
+    }
+    else{
+        return (`It is a string : ${para}`)
+    }
+}
+
+//calling implementation
+console.log(getdatascore("World"));
+console.log(getdatascore(1000));
+
