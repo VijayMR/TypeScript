5bf171a2aa2b82ed34f7d78e07e0284e6f476759
Vijay MR <vijay.mr@tavant.com>
2026-07-09 19:53:35 +0530

    UpdatedCallBackfunction

--- tests/CallBackFunction.ts
+++ tests/CallBackFunction.ts
@@ -2,7 +2,7 @@
 
 //Example:
 
-function expected(a:number,b:number,callback:(name:string)=>void,callback1:(number:number)=>void){ 
+function expected(a:number,b:number,callback:(name:string)=>void,callback1:(number:number)=>void,callback2:()=>void){ 
     
     //callback:(name:string)=>void -->Syntax
     //callback is not keyword it is just the variable
@@ -12,15 +12,27 @@ function expected(a:number,b:number,callback:(name:string)=>void,callback1:(numb
     //other functions getting called as
     callback("John")
     callback1(count);
+    callback2();
 }
 
 //call back function1
 function greet(name:string){
     console.log(`The name is ${name}`)
 }
+
 //call back function2
 function greet1(number:number){
     console.log(`The number is ${number}`)
 }
+
+//call back function3
+function greet3(){
+    console.log(`Success`)
+}
+
 //function invoking
-expected(5,5,greet,greet1) //---greet,greet1 is the function name
+expected(5,5,greet,greet1,greet3) //---greet,greet1 is the function name
+
+
+
+
